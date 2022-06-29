9cfbc5bfc27160ea46b0d8f4abe0eb8c4cd95da7
jenna-graham <jclayto2@gmail.com>
2022-06-28 17:16:43 -0700

    business search API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import { getPokemon } from './services/fetch-utils.js';
+import { getPokemon, getYelp } from './services/fetch-utils.js';
 import { useState, useEffect } from 'react';
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
   const [pokemonQuery, setPokemonQuery] = useState([]);
+  const [businesses, setBusinesses] = useState([]);
+  const [yelpQuery, setYelpQuery] = useState([]);
   
   useEffect(() => {
     fetchAndStorePoke();
@@ -15,29 +17,49 @@ function App() {
     const data = await getPokemon(pokemonQuery);
     setPokemon(data.results);
   }
+  async function fetchAndStoreYelp() {
+    const data = await getYelp(yelpQuery);
+    setBusinesses(data.businesses);
+  }
  
-
-
   async function handleSubmit(e) {
     e.preventDefault();
     await fetchAndStorePoke();
     setPokemonQuery('');
   
   }
+  async function handleYelpSubmit(e) {
+    e.preventDefault();
+    await fetchAndStoreYelp();
+    setYelpQuery('');
   
+  }
+
 
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit}>
-        <input onChange={e => setPokemonQuery(e.target.value)} />
-        <button>Search</button>
-      </form>
-      
-      {pokemon.map((poke, i) => <div key={poke.pokemon + i}>
-        <p>{poke.pokemon}</p>
-        <img src={poke.url_image}/>
-      </div>)}
+    <div className='App'>
+      <div className="pokemon-search">
+        <form onSubmit={handleSubmit}>
+          <input onChange={e => setPokemonQuery(e.target.value)} />
+          <button>Search</button>
+        </form>
+        {pokemon.map((poke, i) => <div key={poke.pokemon + i}>
+          <p>{poke.pokemon}</p>
+          <img src={poke.url_image}/>
+        </div>)}
      
+      </div>
+      <div className="yelp-search">
+        <form onSubmit={handleYelpSubmit}>
+          <input onChange={e => setYelpQuery(e.target.value)} />
+          <button>Search</button>
+        </form>
+        {businesses.map((business, i) => <div key={business.name + i}>
+          <p>{business.name}</p>
+         
+        </div>)}
+
+      </div>
     </div>
   );
 }
