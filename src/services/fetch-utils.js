343839e326979dfbbe64fc2106b4ff07f760e761
jenna-graham <jclayto2@gmail.com>
2022-07-12 17:31:27 -0700

    fixes

--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,11 +1,11 @@
 export async function getPokemon(filter) {
-  const rawData = await fetch(`http://localhost:8888/.netlify/functions/pokemon?pokeQuery=${filter}`);
-  const data = await rawData.json(); 
+  const rawData = await fetch(`/.netlify/functions/pokemon?pokeQuery=${filter}`);
+  const data = await rawData.json();
 
   return data;
 }
 export async function getYelp(filter) {
-  const rawData = await fetch(`http://localhost:8888/.netlify/functions/yelp?yelpQuery=${filter}`);
-  const data = await rawData.json(); 
+  const rawData = await fetch(`/.netlify/functions/yelp?yelpQuery=${filter}`);
+  const data = await rawData.json();
   return data;
 }
